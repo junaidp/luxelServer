8231e5de7a1b5497b3bc0a728e23e1b4a3f0bab4
junaid <junaidp@gmail.com>
2024-10-11 00:13:51 +0200

    req updated

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def chat():
 
         # Process chat and get response
         chat_response = doChat(query, group)
-
+        print(chat_response)
         return chat_response
 
     except HTTPException as he:
@@ -66,21 +66,22 @@ def chat():
 def doChat(query, group):
     customer = group['customers'][0]
     print(customer)
+    first_Name = customer.get('firstName', '');
     passions = ', '.join(customer.get('passions', []));
     interests = ', '.join(customer.get('mainInterests', []));
     lifestyle = ', '.join(customer.get('lifestyle', []));
     travelSpan = ', '.join(customer.get('travelSpan', []));
     travelBucket = ', '.join(customer.get('travelBucketList', []));
     dependent = customer['dependents'][0]
-    dependent_Name = ', '.join(dependent.get('firstName', ''));
+    dependent_Name = dependent.get('firstName', '');
     dep_passions = ', '.join(dependent.get('passions', []));
     dep_interests = ', '.join(dependent.get('mainInterests', []));
     dep_lifestyle = ', '.join(dependent.get('lifestyle', []));
     dep_travelSpan = ', '.join(dependent.get('travelSpan', []));
     dep_travelBucket = ', '.join(dependent.get('travelBucketList', []));
 
-    llm = OpenAI(model="gpt-3.5-turbo", temperature=0)
-   # llm = Anthropic(model="claude-3-opus-20240229", temperature=0)
+    # llm = OpenAI(model="gpt-4", temperature=0)
+    llm = Anthropic(model="claude-3-opus-20240229", temperature=0)
     storage_context = StorageContext.from_defaults(persist_dir="storage")
     index = load_index_from_storage(storage_context)
 
@@ -104,18 +105,20 @@ def doChat(query, group):
                 "If a query is not covered by the training data, use the fallback response."
                 "4. Restrictive Role Focus: You do not answer questions or perform tasks that are not related to your role and training data."
                 "5. If you need more info to help customer find their relevant experiences, Do ask them further questions which can help you to suggest."
-                "6. Always Welcome user's with their first name."
+                "6. Always start the conversation with a friendly and cheerful way and greet with  'Hi, "+ first_Name+
                 "7. If Number of days is not mentioned by the user, Do ask them, And then accordingly suggest them several related experiences which can be covered in that time span, Also in the sequence which could be easily done according to the location of each experience."
                 "For example cover 2 experiences in one day which are located close enough , and so on. Always first ask the user for no of days and only then Suggest them experiences."
                 "8. Always consider that customer is traveling near Paris, and don’t ask this question."
                 "9. Only suggest the experiences which are in the context.Do not suggest anything which is not the in the context"
-                "10. Suggest ALL the experiences which have in these interests: " + interests + ",  Passions i.e " + passions + ",  lifestyle i.e " + lifestyle + ". Also consider the travelSpan i.e " + travelSpan + " and my travelBucket List i.e: " + travelBucket + ""
-                 "11. Also show page Number with every suggested experience , on the page where that experience is in pdf"
-                 "12. For every suggested experience look for its ID on that same page , and write that as well in your response"
-                 "13. For every suggested experience ,Also mention Why you suggest this experience , i.e Which interest aligns with this experience"
-                 "14. You must provide the 'ID' and its respective 'Title' fields of the suggested experiences,  Exactly as they are written in the context. Your given ID and Title must be available in the document "
-                 "Here are the relevant documents for the context:\n"
-                 "{context_str}"                                                                                                                                                                                                                                                         "  dependent i.e" + dependent_Name + "'s interests , i.e " + dep_interests + ", " + dependent_Name + "'s Passions i.e " + dep_passions + ", " + dependent_Name + "'s lifestyle i.e " + dep_lifestyle + ". Also consider " + dependent_Name + "'s travelSpan i.e " + dep_travelSpan + " and " + dependent_Name + "'s travelBucket List i.e: " + dep_travelBucket + ""
+                "10. Suggest ALL the experiences where you see the keywords that matches with my interests: " + interests + ",  Passions i.e " + passions + ",  lifestyle i.e " + lifestyle + ". Also consider the travelSpan i.e " + travelSpan + " and my travelBucket List i.e: " + travelBucket + ""
+                "11. Do Not ask for for passions or interests or any related info of the user, as you already know them from above"
+                "12. Also show page Number with every suggested experience , on the page where that experience is in pdf"
+                "13. For every suggested experience look for its ID on that same page , and write that as well in your response"
+                "14. For every suggested experience ,Also mention Why you suggest this experience , i.e Which interest aligns with this experience"
+                "15. You must provide the 'ID' and its respective 'Title' fields of the suggested experiences,  Exactly as they are written in the context. Your given ID and Title must be available in the document "
+                "16. If there are dependents, try to suggest some experience for the dependents as and mention that experience , aligns with "+ dependent_Name+",s " 
+                "Here are the relevant documents for the context:\n"
+                "{context_str}"                                                                                                                                                                                                                                                         "  dependent i.e" + dependent_Name + "'s interests , i.e " + dep_interests + ", " + dependent_Name + "'s Passions i.e " + dep_passions + ", " + dependent_Name + "'s lifestyle i.e " + dep_lifestyle + ". Also consider " + dependent_Name + "'s travelSpan i.e " + dep_travelSpan + " and " + dependent_Name + "'s travelBucket List i.e: " + dep_travelBucket + ""
         ),
         verbose=False,
     )
